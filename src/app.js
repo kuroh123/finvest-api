44992c9eba90a3cc1ad09c4a0318d5a0dfaf9d04
Obaid <Obaidurrahman031@gmail.com>
2025-09-20 15:19:32 +0530

    FIX: Changes cors policy#2

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ const allowedOrigins = ["https://finvest.obaidur.dev"];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
